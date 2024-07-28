3168142e6ed7659d85e75ae1ec350576a86c5673
Dang Hai Huy <haihuy9a@gmail.com>
2024-07-28 18:46:56 +0700

    task/huy/fixWishListApi

--- src/main/java/com/loco/demo/repository/WishList/WishListRepo.java
+++ src/main/java/com/loco/demo/repository/WishList/WishListRepo.java
@@ -10,4 +10,6 @@ import com.loco.demo.entity.WishList;
 @Repository
 public interface WishListRepo extends JpaRepository<WishList,String>{
     public Optional<WishList> findByHotelId(String id);
+
+    public boolean existsByHotelId(String id);
 }
